29969672d06fef753ac61bfe93e3d82521fc197c
Tomas178 <tomaspetronis77@gmail.com>
2026-03-08 13:44:49 +0200

    First version of Sigmoid Neuron implmentation

--- 2Lab/Utils/SigmoidNeuron.py
+++ 2Lab/Utils/SigmoidNeuron.py
@@ -14,12 +14,10 @@ class SigmoidNeuron:
 		self.e_min = e_min
 		self.seed = seed
 
-		# Pradiniai svoriai bei bias'ai iš intervalo [INTERVAL_START, INTERVAL_END]
 		np.random.seed(seed)
 		self.weights = np.random.uniform(self.INTERVAL_START, self.INTERVAL_END, n_features)
 		self.bias = np.random.uniform(self.INTERVAL_START, self.INTERVAL_END)
 
-		# Metrikų sėkimui
 		self.train_errors = []
 		self.validation_errors = []
 		self.train_accuracies = []
@@ -34,18 +32,84 @@ class SigmoidNeuron:
 		a = np.dot(X, self.weights) + self.bias
 		return self._sigmoid(a)
 
-	def classify(self, X: NDArray[np.float64]) -> NDArray[np.float64]:
+	def classify(self, X: np.ndarray) -> np.ndarray:
 		return np.round(self.predict(X)).astype(int)
 
-	def _compute_error(self, y_pred: NDArray[np.float64], y_true: NDArray[np.float64]) -> float:
-		return np.sum((y_true, y_pred) ** 2)
+	def _compute_error(self, y_pred: np.ndarray, y_true: np.ndarray) -> float:
+		return np.sum((y_true - y_pred) ** 2)
 
-	def _compute_accuracy(self, y_pred: NDArray[np.float64], y_true: NDArray[np.float64]) -> float:
+	def _compute_accuracy(self, y_pred: np.ndarray, y_true: np.ndarray) -> float:
 		return np.mean(np.round(y_pred).astype(int) == y_true)
 
 	def _reset_metrics(self):
 		self.train_errors = []
 		self.validation_errors = []
-
 		self.train_accuracies = []
 		self.validation_accuracies = []
+
+	def _record_metrics(self, X_train, y_train, X_val, y_val, train_error):
+		self.train_errors.append(train_error)
+		self.train_accuracies.append(self._compute_accuracy(self.predict(X_train), y_train))
+
+		y_pred_val = self.predict(X_val)
+		self.validation_errors.append(self._compute_error(y_pred_val, y_val))
+		self.validation_accuracies.append(self._compute_accuracy(y_pred_val, y_val))
+
+	def _train(self, X_train, y_train, X_val, y_val, stochastic=False):
+		self._reset_metrics()
+		start_time = time.time()
+
+		total_error = float('inf')
+		epoch = 0
+
+		while total_error > self.e_min and epoch < self.epochs:
+			indices = np.random.permutation(len(X_train))
+			X_shuffled = X_train[indices]
+			y_shuffled = y_train[indices]
+
+			total_error = 0.0
+			gradient_sum_w = np.zeros(len(self.weights))
+			gradient_sum_b = 0.0
+
+			for i in range(len(X_shuffled)):
+				x_i = X_shuffled[i]
+				t_i = y_shuffled[i]
+				y_i = self.predict(x_i.reshape(1, -1))[0]
+
+				grad = (y_i - t_i) * y_i * (1 - y_i)
+
+				if stochastic:
+					# Svoriai atnaujinami po kiekvieno pavyzdžio
+					self.weights -= self.learning_rate * grad * x_i
+					self.bias -= self.learning_rate * grad
+				else:
+					# Gradientai kaupiami per visą epochą
+					gradient_sum_w += grad * x_i
+					gradient_sum_b += grad
+
+				total_error += (t_i - y_i) ** 2
+
+			if not stochastic:
+				# Svoriai atnaujinami vieną kartą per epochą
+				m = len(X_shuffled)
+				self.weights -= self.learning_rate * (gradient_sum_w / m)
+				self.bias -= self.learning_rate * (gradient_sum_b / m)
+
+			self._record_metrics(X_train, y_train, X_val, y_val, total_error)
+			epoch += 1
+
+		self.epochs_run = epoch
+		self.training_time = time.time() - start_time
+
+	def train_batch(self, X_train, y_train, X_val, y_val):
+		self._train(X_train, y_train, X_val, y_val, stochastic=False)
+
+	def train_stochastic(self, X_train, y_train, X_val, y_val):
+		self._train(X_train, y_train, X_val, y_val, stochastic=True)
+
+	def evaluate(self, X: np.ndarray, y: np.ndarray):
+		y_pred = self.predict(X)
+		error = self._compute_error(y_pred, y)
+		accuracy = self._compute_accuracy(y_pred, y)
+		predictions = self.classify(X)
+		return error, accuracy, predictions, y_pred
